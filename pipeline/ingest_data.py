60cfc654cd4bbe6b5f94d9534b62fa0f802e8e76
masha <33293182+Marija97@users.noreply.github.com>
2026-08-27 15:40:21 +0000

    use click to make the script params configurable through command line interface

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -3,6 +3,7 @@
 
 
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -33,22 +34,29 @@ parse_dates = [
 ]
 
 
-def run():
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021,  type=int, help='Year of the data')
+@click.option('--month', default=1,  type=int, help='Month of the data')
+@click.option('--chunksize', default=100000,  type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run (pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     """Ingest NYC taxi data into PostgreSQL database."""
 
     # Prepare data url
-    year, month = 2021, 1
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
 
     # Create Database connection
-    pg_user, pg_pass, pg_host, pg_port, pg_db = 'root', 'root', 'localhost', 5432, 'ny_taxi'
     engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
 
     # Read all of the available data with parsed types into an iterator of data chunks
-    chunksize = 100000
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
@@ -58,7 +66,6 @@ def run():
     )
    
     # Ingest data into target table in chunks with a progress bar 
-    target_table = 'yellow_taxi_data'
     first = True
 
     for df_chunk in tqdm(df_iter):
@@ -69,6 +76,7 @@ def run():
 
         # Insert the data chunk into the target table
         df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
+
 
 
 
