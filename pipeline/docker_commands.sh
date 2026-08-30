15316f3bd169e067a2d94ad8524235631be41ba8
masha <33293182+Marija97@users.noreply.github.com>
2026-08-30 13:17:22 +0000

    create postgres and pgadmin containers on the same network

--- pipeline/docker_commands.sh
+++ pipeline/docker_commands.sh
@@ -6,3 +6,26 @@ docker network rm pg-network
 
 # look at the existing networks with 
 docker network ls
+
+# Run PostgreSQL on the network --> pgdatabase
+docker run -it --rm \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v ny_taxi_postgres_data:/var/lib/postgresql \
+  -p 5432:5432 \
+  --network=pg-network \
+  --name pgdatabase \
+  postgres:18
+
+
+# In another terminal, run pgAdmin on the same network --> pgadmin
+docker run -it --rm \
+  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+  -e PGADMIN_DEFAULT_PASSWORD="root" \
+  -v pgadmin_data:/var/lib/pgadmin \
+  -p 8085:80 \
+  --network=pg-network \
+  --name pgadmin \
+  dpage/pgadmin4
+
