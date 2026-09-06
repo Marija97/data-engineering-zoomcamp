fd9f0c2a282977249ce3e728a09c319f846d45ac
Marija97 <marija.vujica97@gmail.com>
2026-09-06 14:40:15 +0200

    use Terraform to create a GC bucket

--- 01-docker-terraform/terraform-gcp/terrademo/main.tf
+++ 01-docker-terraform/terraform-gcp/terrademo/main.tf
@@ -11,3 +11,18 @@ provider "google" {
   project = "dtc-de-course-507510"
   region  = "europe-west2"
 }
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "dtc-de-course-507510-terrabucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
